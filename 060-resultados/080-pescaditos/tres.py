8d38cf5aff4c3a17d12d2c4b28c6c9af743ee5af
jeremy theler <jeremy.theler@onscale.com>
2023-10-22 21:56:56 -0300

    nicer

--- 060-resultados/080-pescaditos/tres.py
+++ 060-resultados/080-pescaditos/tres.py
@@ -6,9 +6,8 @@ import subprocess
 def keff(x):
   r = subprocess.run(["bash", "tres.sh", "(%s)" % str(x[0]), "(%s)" % str(x[1])], stdout=subprocess.PIPE)
   k = float(r.stdout.decode('utf-8'))
-  print("%g\t%g\t%g" % (x[0], x[1], k))
   return k
 
 x0 = [+25, -25]
-result = minimize(keff, x0, method="nelder-mead", options = {"fatol": 1e-5, "xatol": 1})
+result = minimize(keff, x0, method="nelder-mead")
 print(result)
